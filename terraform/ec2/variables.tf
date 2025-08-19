8fa0207d4cae271316b56fb0c3000f1af13eacdc
Aditya-711 <64785580+Aditya-711@users.noreply.github.com>
2025-08-19 14:30:40 +0530

    Update variables.tf

--- terraform/ec2/variables.tf
+++ terraform/ec2/variables.tf
@@ -1,1 +1,15 @@
+variable "region" {
+  default = "us-east-1"
+}
 
+variable "ami_id" {
+  default = "ami-08c40ec9ead489470" # Amazon Linux 2 in us-east-1
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "instance_name" {
+  default = "MyEC2Instance"
+}
